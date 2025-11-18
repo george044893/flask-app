037e2f20961f79f109372a1bad1a8910d77d467d
George Wong <george044893@gmail.com>
2025-11-17 21:34:20 -0500

    update version
    Signed-off-by: George Wong <george044893@gmail.com>

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 @app.route("/")
 def main():
     hostname = socket.gethostname()
-    return f"You're home now on {hostname} of version 1!"
+    return f"You're home now on {hostname} of version 2!"
 @app.route('/hello-world')
 def hello_world():
     return 'Hello World'
